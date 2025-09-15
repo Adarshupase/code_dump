55a13be7e9eb875f7080a8c0c2898ca12329a181
Adarsh Upase <138318435+Adarshupase@users.noreply.github.com>
2025-09-15 21:55:08 +0530

    Update longest_consecutive_subsequence.cpp
    Done

--- arrays/longest_consecutive_subsequence.cpp
+++ arrays/longest_consecutive_subsequence.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int longestConsecutiveSub(vector<int>& nums){
+    // the problem is to find the longest consecutive subsequence
+    //start from creating a set out of it 
+    // set<int> would work fine as it is already sorted but we will use unordered set
+    unordered_set<int> st(nums.begin(),nums.end());
     
+    int ans = 0;
+    for(auto num : st) {
+        if(st.find(num-1) == st.end()){
+            // meaning there are no preceding elements for this number start the count from here 
+            int count = 0;
+            while(st.find(num) != st.end()){
+                count++;
+                num++;
+            }
+            ans = max(count,ans);
+        }
+    }
+    return ans;
+        
 }
